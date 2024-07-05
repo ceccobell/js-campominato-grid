627dc5e933bef13ff8ec4cbba90978c2d24851c6
ceccobell <francescobell2001@gmail.com>
2024-07-05 15:09:32 +0200

    [UPD] added button to change difficulty

---
+++ bonus/js/script.js
@@ -0,0 +1,21 @@
+// recupero il bottone play e gli do la funzione
+
+document.getElementById("play").addEventListener("click", function () {
+    // recupero l'elemento grid in cui mettere i quadrati
+    const grid = document.getElementById("grid")
+
+    // ciclo for per generare tot quadrati
+    for (let i = 0; i < 100; i++) {
+        let square = document.createElement("div");
+        square.classList.add("square");
+        grid.append(square)
+        let content = document.createElement("span");
+        square.append(content);
+        content.textContent = i + 1;
+
+        square.addEventListener("click", function () {
+            this.classList.toggle("bg-azure")
+            console.log("Hai cliccato la", i + 1, "cella")
+        })
+    }
+})
