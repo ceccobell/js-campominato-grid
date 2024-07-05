e744ffbcfb15687ab0be791a471c83cfa0ae756e
ceccobell <francescobell2001@gmail.com>
2024-07-05 15:36:45 +0200

    [UPD] bonus done

--- bonus/js/script.js
+++ bonus/js/script.js
@@ -1,21 +1,37 @@
-// recupero il bottone play e gli do la funzione
+const grid = document.getElementById("grid")
 
+// recupero il bottone play e gli do la funzione
 document.getElementById("play").addEventListener("click", function () {
     // recupero l'elemento grid in cui mettere i quadrati
-    const grid = document.getElementById("grid")
+    let difficulty = document.getElementById("difficulty").value
 
-    // ciclo for per generare tot quadrati
-    for (let i = 0; i < 100; i++) {
+    if (difficulty == "hard") {
+        createSquare(49, "hard")
+    } else if (difficulty == "medium") {
+        createSquare(81, "medium")
+    } else if (difficulty == "easy") {
+        createSquare(100, "easy")
+    }
+})
+
+function createSquare(value, size) {
+    for (let i = 0; i < value; i++) {
+
         let square = document.createElement("div");
-        square.classList.add("square");
+        square.classList.add("square", size);
         grid.append(square)
+
         let content = document.createElement("span");
         square.append(content);
         content.textContent = i + 1;
 
-        square.addEventListener("click", function () {
-            this.classList.toggle("bg-azure")
-            console.log("Hai cliccato la", i + 1, "cella")
-        })
+        changeColor(square, i)
     }
-})
+}
+
+function changeColor(square, i) {
+    square.addEventListener("click", function () {
+        this.classList.toggle("bg-azure")
+        console.log("Hai cliccato la", i + 1, "cella")
+    })
+}
